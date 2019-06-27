9ba3719a71c79bac4c4e5440430144c78a739f84
Duane Appleby <applebyd@uk.ibm.com>
2019-06-27 11:07:29 +0100

    Enable npm tests in prow on webhooks extension

--- webhooks-extension/test-it.sh
+++ webhooks-extension/test-it.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
 # Workaround since with React as imports, we get no bundle from rollup.
+exit_code=0
 npm install --save-dev react
-
 
 if [[ $# -eq 1 ]] ; then
   echo "Arg provided to test: ${1}"
-  jest ${1}
+  if [[ ${1} == "ci" ]] ; then
+    jest
+    exit_code=$?
+  else 
+    jest ${1}
+    exit_code=$?
+  fi
 else
   jest --watchAll
 fi
 
 npm uninstall --save-dev react
+exit $exit_code 
