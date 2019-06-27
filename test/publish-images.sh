b07938c750d5e8882cfc7c71e5a8ddbeca21188c
Vincent Demeester <vdemeest@redhat.com>
2019-06-27 10:10:51 +0200

    Add script to publish experimental images
    It will call all project's `publish-images.sh` script.
    - tekton-listener publishes `controller` and `tekton-listener` images
    - webhooks-extension publishes `sink` and `extension` images
    
    Signed-off-by: Vincent Demeester <vdemeest@redhat.com>

---
+++ test/publish-images.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Copyright 2018 The Tekton Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This script runs at the postsubmit phase; it is started by prow when a push event
+# happens on master, via a PR merge for example.
+
+./webhooks-extension/publish-images.sh
+./tekton-listener/publish-images.sh
