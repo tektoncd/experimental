ce49aa1fa8504dca01ebd6c26ad830375c7b97c5
Mark Nuttall <mnuttall@uk.ibm.com>
2019-06-27 16:16:59 +0100

    20x50

--- webhooks-extension/pkg/endpoints/webhook_test.go
+++ webhooks-extension/pkg/endpoints/webhook_test.go
@@ -531,8 +531,8 @@ func TestMultipleDeletesCorrectData(t *testing.T) {
 	t.Log("in TestMultipleDeletesCorrectData")
 	r := setUpServer()
 
-	numTimes := 1
-	numHooks := 40
+	numTimes := 20
+	numHooks := 50
 	runtime.GOMAXPROCS(2)
 
 	for i := 0; i < numTimes; i++ {
