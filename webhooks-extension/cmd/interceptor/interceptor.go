07ee8d882f93448b9bcbfd36a0d9d4c23eae9efd
Duane Appleby <applebyd@uk.ibm.com>
2020-01-29 10:45:47 +0000

    Updates webhooks extension eventlistener to be compatible with triggers 0.2.1.

--- webhooks-extension/cmd/interceptor/interceptor.go
+++ webhooks-extension/cmd/interceptor/interceptor.go
@@ -15,14 +15,15 @@ package main
 
 import (
 	"fmt"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
 	"strings"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
 
 const (
@@ -32,6 +33,14 @@ const (
 func main() {
 	log.Print("Interceptor started")
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+
+		// Copy Headers from the request onto the response
+		for k, valueArray := range request.Header {
+			for _, v := range valueArray {
+				writer.Header().Add(k, v)
+			}
+		}
+
 		foundTriggerName := request.Header.Get("Wext-Trigger-Name")
 		config, err := rest.InClusterConfig()
 		if err != nil {
