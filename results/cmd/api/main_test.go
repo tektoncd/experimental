85cf1021d69ac923345843075a9a446201e6324a
yuege01 <f1997214@hotmail.com>
2020-06-15 23:26:26 -0700

    Implement GetTaskRun() v1 Open issue #560 Create a GetTaskRun() to enable user get an exsiting TaskRun from database
    Modified Tests and styling
    fixing issue #560
    mainly fixed styling issues
    
    Fixed taskrun schema and test
    Fixing issue #560
    Changed type of uid to be Text in database, so it has the same type as in taskrun proto
    Added a test to check the equality of Created taskrun and Retrieved taskrun with same uid
    
    Added changes in go.sum
    Fixing issue #560
    
    Modified Comment Typo
    Fixing issue #560
    
    Modified tests and api server file
    fixing issue #560
    Used cmp.Diff to check equality of two taskruns so that users can log difference if they are different
    
    Fixed format issue
    fixing issue #560
    re-formatted code format of main_test.go so it passes new test
    
    Resolving conflicts in results.sql and delete unused import in go.mod
    fixing issue #560

--- results/cmd/api/main_test.go
+++ results/cmd/api/main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"database/sql"
+	"github.com/google/go-cmp/cmp"
 	"io/ioutil"
 	"os"
 	"testing"
@@ -18,25 +19,26 @@ const (
 // Test functionality of Server code
 func TestCreateTaskRun(t *testing.T) {
 	// Create a temporay database
-	testDB, err := setupTestDB("testdb", t)
+	srv, err := setupTestDB("testdb", t)
 	if err != nil {
 		t.Fatalf("failed to create temp file for db: %v", err)
 	}
 
 	// connect to fake server and do testing
-	srv := &server{db: testDB}
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
-	objectmeta := &pb.ObjectMeta{Uid: "123459", Name: "mytaskrun", Namespace: "default"}
-	r, err := srv.CreateTaskRun(ctx, &pb.CreateTaskRunRequest{TaskRun: &pb.TaskRun{ApiVersion: "1", Metadata: objectmeta}})
-	if err != nil {
+	if _, err := srv.CreateTaskRun(ctx, &pb.CreateTaskRunRequest{
+		TaskRun: &pb.TaskRun{ApiVersion: "1",
+			Metadata: &pb.ObjectMeta{
+				Uid:       "123459",
+				Name:      "mytaskrun",
+				Namespace: "default"}}}); err != nil {
 		t.Fatalf("could not create taskrun: %v", err)
 	}
-	t.Logf("Created taskrun: %s", r.String())
 }
 
 // setupTestDB set up a temporary database for testing
-func setupTestDB(dbName string, t *testing.T) (*sql.DB, error) {
+func setupTestDB(dbName string, t *testing.T) (*server, error) {
 	// Create a temporary file
 	tmpfile, err := ioutil.TempFile("", "testdb")
 	if err != nil {
@@ -45,6 +47,7 @@ func setupTestDB(dbName string, t *testing.T) (*sql.DB, error) {
 
 	// Connect to sqlite DB.
 	db, err := sql.Open("sqlite3", tmpfile.Name())
+	srv := &server{db: db}
 	if err != nil {
 		t.Fatalf("failed to open the results.db: %v", err)
 	}
@@ -54,12 +57,45 @@ func setupTestDB(dbName string, t *testing.T) (*sql.DB, error) {
 	})
 
 	// Create taskrun table
-	statement, err := db.Prepare("CREATE TABLE IF NOT EXISTS taskrun (logid binary(16) PRIMARY KEY, taskrunlog BLOB, uid INTEGER, name TEXT, namespace TEXT)")
+	statement, err := db.Prepare("CREATE TABLE IF NOT EXISTS taskrun (logid binary(16) PRIMARY KEY, taskrunlog BLOB, uid TEXT, name TEXT, namespace TEXT)")
 	if err != nil {
 		t.Fatalf("failed to create taskrun table: %v", err)
 	}
 	if _, err := statement.Exec(); err != nil {
 		t.Fatalf("failed to execute the taskrun table creation statement statement: %v", err)
 	}
-	return db, nil
+	return srv, nil
+}
+
+func TestGetTaskRun(t *testing.T) {
+	// Create a temporary database
+	srv, err := setupTestDB("testdb", t)
+	if err != nil {
+		t.Fatalf("failed to setup db: %v", err)
+	}
+
+	// Connect to fake server and create a new taskrun
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := srv.CreateTaskRun(ctx, &pb.CreateTaskRunRequest{
+		TaskRun: &pb.TaskRun{
+			ApiVersion: "v1beta1",
+			Metadata: &pb.ObjectMeta{
+				Uid:       "31415926",
+				Name:      "mytaskrun",
+				Namespace: "default",
+			}}})
+	if err != nil {
+		t.Fatalf("could not create taskrun: %v", err)
+	}
+	t.Logf("Created taskrun: %s", r.String())
+
+	// Test if we can find inserted taskrun
+	res, err := srv.GetTaskRun(ctx, &pb.GetTaskRunRequest{Uid: r.GetMetadata().GetUid()})
+	if err != nil {
+		t.Fatalf("could not get taskrun: %v", err)
+	}
+	if diff := cmp.Diff(r.String(), res.String()); diff != "" {
+		t.Fatalf("could not get the same taskrun: %v", diff)
+	}
 }
--- results/cmd/api/results.sql
+++ results/cmd/api/results.sql
@@ -1,2 +1,4 @@
+-- logid serve as auto-generated key by sqltite to uniquely identify a taskrun log
+-- uid is the key from taskrun itself we now use to do query on taskrun logs, future we may use cluster-info as key
 .open results.db
-CREATE TABLE taskrun (logid binary(16) PRIMARY KEY, taskrunlog BLOB, uid INTEGER, name TEXT, namespace TEXT);
+CREATE TABLE taskrun (logid binary(16) PRIMARY KEY, taskrunlog BLOB, uid TEXT, name TEXT, namespace TEXT);
