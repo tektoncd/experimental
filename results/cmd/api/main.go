d6e28d6c5a0b1f24c2fb40291090680c6892c36a
Billy Lynch <wlynch@google.com>
2020-06-29 17:59:56 -0400

    Revert "Connect to a local MySQL instance to store results"
    This reverts commit de1eae3f1488447ad439d7f18f60b51b97fc8b16.
    
    Because presubmit tests were not in place (adding in #578) we did not
    catch issues around mysql / sqlite3 compatibility. Rolling back to a
    more stable state so we can get the presubmit tests in, then we can
    rollforward.

--- results/cmd/api/main.go
+++ results/cmd/api/main.go
@@ -19,63 +19,28 @@ package main
 import (
 	"context"
 	"database/sql"
-	"flag"
 	"fmt"
 	"log"
 	"net"
-	"os"
 
-	_ "github.com/go-sql-driver/mysql"
 	"github.com/golang/protobuf/proto"
+	_ "github.com/mattn/go-sqlite3"
 	pb "github.com/tektoncd/experimental/results/proto/proto"
 	"google.golang.org/grpc"
 )
 
-const (
-	dbname     = "tekton-results"
-	dbprotocol = "tcp"
-)
-
-var dbaddr = flag.String("db_addr", "mysql.tekton-pipelines.svc.cluster.local", "Address of MySQL database to use.")
+const port = ":50051"
 
 func main() {
-	flag.Parse()
-
-	user, pass := os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD")
-	if user == "" || pass == "" {
-		log.Fatal("Must provide both DB_USER and DB_PASSWORD")
-	}
-
-	if *dbaddr == "" {
-		log.Fatal("Must provide --db_addr")
-	}
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		// Default gRPC server port to this value from tutorials (e.g., https://grpc.io/docs/guides/auth/#go)
-		port = ":50051"
-	}
-
-	// Connect to the MySQL database.
-	// DSN derived from https://github.com/go-sql-driver/mysql#dsn-data-source-name
-	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@%s(%s)/", user, pass, dbprotocol, *dbaddr))
+	// Connect to sqlite DB.
+	db, err := sql.Open("sqlite3", "./results.db")
 	if err != nil {
 		log.Fatalf("failed to open the results.db: %v", err)
 	}
 	defer db.Close()
-
-	// Sanity check that we can connect to MySQL.
-	if _, err := db.Exec("SHOW databases;"); err != nil {
-		log.Fatalf("show databases: %v", err)
-	}
-
-	// Initialize the database if it doesn't exist.
-	if _, err := db.Exec("CREATE DATABASE IF NOT EXISTS `tekton-results`;"); err != nil {
-		log.Fatalf("create database: %v", err)
-	}
+	srv := &server{db: db}
 
 	// Listen for gRPC requests.
-	srv := &server{db: db}
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
