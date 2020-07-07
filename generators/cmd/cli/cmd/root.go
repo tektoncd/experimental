a5b2f4327fcf3f06b501073d6d82918077f1a9d4
yolandadu <yolandadu@google.com>
2020-07-07 11:00:47 -0500

    Add support for applying and deleting configs on cluster
    This commit is to add support to apply generated configs on cluster and
    then also enable users to delete them from cluster. Use subtests to test
    different cases.

--- generators/cmd/cli/cmd/root.go
+++ generators/cmd/cli/cmd/root.go
@@ -3,15 +3,28 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
+	"k8s.io/client-go/util/homedir"
 )
 
-var rootCmd = &cobra.Command{
-	Use:   "tkn-gen",
-	Short: "This is the CLI for tekton generators",
-	Long: `tkn-gen will allow you to generate Tekton spec from the given simple spec. 
+var (
+	kubeconfig string
+	rootCmd    = &cobra.Command{
+		Use:   "tkn-gen",
+		Short: "This is the CLI for tekton generators",
+		Long: `tkn-gen will allow you to generate Tekton spec from the given simple spec. 
 			It also contains support for managing generated configuration.`,
+	}
+)
+
+func init() {
+	if home := homedir.HomeDir(); home != "" {
+		rootCmd.PersistentFlags().StringVarP(&kubeconfig, "kubeconfig", "k", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		rootCmd.PersistentFlags().StringVarP(&kubeconfig, "kubeconfig", "k", "", "absolute path to the kubeconfig file")
+	}
 }
 
 func Execute() {
