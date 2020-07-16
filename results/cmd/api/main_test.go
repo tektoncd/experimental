b90c44a61eb434561421e31fe24270d48583dd92
ziweifan <ziweifan@google.com>
2020-07-16 10:09:09 -0700

    Add fieldmask utility to UpdateTaskRun to only update specific fields in TaskRun Added a FieldMask to UpdateTaskRunRequest proto message Modified Api Server main.go to use fieldmask for updating Added tests to test if specified fields are updated and unspecified fields are untouched
    Refactoring code and add more tests
    Fix Issue #593
    Created a helper function() for common codes in GetTaskRunResult and UpdateTaskRunResult
    Fixed fieldmask naming issue in api.proto
    Added Concurrency to update on database
    Added error checking in main.go
    Created more tests for UpdateTaskRun including tests for no fieldmask, fieldmask with partial field,excess field, empty field, invalid field, field mask updating repeated field
    
    Added test for fieldmask with nil Paths field.
    Fixing issue #593
    Update Request including fieldmask with nil paths will not update taskrun
    
    Adding transaction to READ and fixing style issues
    Fixing issue #593
    Add transaction to both read and write to database
    Fix error handling issue
    change GetTaskRunById() to a private function
    
    Use fieldmask pointer in the testing
    Fixing issue #593

--- results/cmd/api/main_test.go
+++ results/cmd/api/main_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/golang/protobuf/proto"
 	"github.com/google/go-cmp/cmp"
 	pb "github.com/tektoncd/experimental/results/proto/proto"
+	"google.golang.org/genproto/protobuf/field_mask"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -118,37 +119,282 @@ func TestUpdateTaskRun(t *testing.T) {
 		t.Fatalf("failed to create temp file for db: %v", err)
 	}
 
-	// Connect to fake server and create a taskrun
+	// Connect to fake server
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
-	objectmeta := &pb.ObjectMeta{Uid: "123459", Name: "mytaskrun", Namespace: "default"}
-	r, err := srv.CreateTaskRunResult(ctx, &pb.CreateTaskRunRequest{TaskRun: &pb.TaskRun{ApiVersion: "1", Metadata: objectmeta}})
-	if err != nil {
-		t.Fatalf("could not create taskrun: %v", err)
-	}
 
-	// Update the created taskrun
-	objectmeta = &pb.ObjectMeta{Uid: "123459", Name: "ziweifan", Namespace: "tomorrow"}
-	r, err = srv.UpdateTaskRunResult(ctx, &pb.UpdateTaskRunRequest{TaskRun: &pb.TaskRun{ApiVersion: "v1alpha1", Metadata: objectmeta}, ResultsId: r.GetResultsId()})
-	if err != nil {
-		t.Fatalf("could not update taskrun: %v", err)
+	// Validate by checking if ouput equlas expected TaskRun
+	tt := []struct {
+		name      string
+		in        *pb.TaskRun
+		fieldmask *field_mask.FieldMask
+		update    *pb.TaskRun
+		expect    *pb.TaskRun
+	}{
+		{
+			in: &pb.TaskRun{
+				ApiVersion: "v1alpha1",
+				Metadata: &pb.ObjectMeta{
+					Uid:       "31415926",
+					Name:      "mytaskrun",
+					Namespace: "default",
+				},
+			},
+			name: "Test no Mask",
+			update: &pb.TaskRun{
+				ApiVersion: "v1beta1",
+				Metadata: &pb.ObjectMeta{
+					Uid:       "123456",
+					Name:      "newtaskrun",
+					Namespace: "tekton",
+				},
+			},
+			// update entire taskrun
+			expect: &pb.TaskRun{
+				ApiVersion: "v1beta1",
+				Metadata: &pb.ObjectMeta{
+					Uid:       "123456",
+					Name:      "newtaskrun",
+					Namespace: "tekton",
+				},
+			},
+		},
+		{
+			in: &pb.TaskRun{
+				ApiVersion: "v1alpha1",
+				Metadata: &pb.ObjectMeta{
+					Uid:       "31415926",
+					Name:      "mytaskrun",
+					Namespace: "default",
+				},
+			},
+			name:      "Test partial Mask",
+			fieldmask: &field_mask.FieldMask{Paths: []string{"api_version", "metadata.uid"}},
+			update: &pb.TaskRun{
+				ApiVersion: "v1beta1",
+				Metadata: &pb.ObjectMeta{
+					Uid:       "123456",
+					Name:      "newtaskrun",
+					Namespace: "tekton",
+				},
+			},
+			// update fields in fieldmask only
+			expect: &pb.TaskRun{
+				ApiVersion: "v1beta1",
+				Metadata: &pb.ObjectMeta{
+					Uid:       "123456",
+					Name:      "mytaskrun",
+					Namespace: "default",
+				},
+			},
+		},
+		{
+			in: &pb.TaskRun{
+				ApiVersion: "v1alpha1",
+				Metadata: &pb.ObjectMeta{
+					Uid:       "31415926",
+					Name:      "mytaskrun",
+					Namespace: "default",
+				},
+			},
+			name:      "Test Mask with excess field",
+			fieldmask: &field_mask.FieldMask{Paths: []string{"api_version", "metadata.uid"}},
+			// unset field value to default value in fieldmask
+			update: &pb.TaskRun{
+				Metadata: &pb.ObjectMeta{
+					Uid:       "123456",
+					Name:      "newtaskrun",
+					Namespace: "tekton",
+				},
+			},
+			expect: &pb.TaskRun{
+				Metadata: &pb.ObjectMeta{
+					Uid:       "123456",
+					Name:      "mytaskrun",
+					Namespace: "default",
+				},
+			},
+		},
+		{
+			in: &pb.TaskRun{
+				ApiVersion: "v1alpha1",
+				Metadata: &pb.ObjectMeta{
+					Uid:       "31415926",
+					Name:      "mytaskrun",
+					Namespace: "default",
+				},
+			},
+			name:      "Test Mask with invalid field",
+			fieldmask: &field_mask.FieldMask{Paths: []string{"api_version", "invalid_field", "metadata.uid"}},
+			// do not update
+			update: &pb.TaskRun{
+				ApiVersion: "v1beta1",
+				Metadata: &pb.ObjectMeta{
+					Uid:       "123456",
+					Name:      "newtaskrun",
+					Namespace: "tekton",
+				},
+			},
+			expect: &pb.TaskRun{
+				ApiVersion: "v1alpha1",
+				Metadata: &pb.ObjectMeta{
+					Uid:       "31415926",
+					Name:      "mytaskrun",
+					Namespace: "default",
+				},
+			},
+		},
+		{
+			in: &pb.TaskRun{
+				ApiVersion: "v1alpha1",
+				Metadata: &pb.ObjectMeta{
+					Uid:       "31415926",
+					Name:      "mytaskrun",
+					Namespace: "default",
+				},
+			},
+			name:      "Test Mask with empty field",
+			fieldmask: &field_mask.FieldMask{Paths: []string{}},
+			// do not update
+			update: &pb.TaskRun{
+				ApiVersion: "v1beta1",
+				Metadata: &pb.ObjectMeta{
+					Uid:       "123456",
+					Name:      "newtaskrun",
+					Namespace: "tekton",
+				},
+			},
+			expect: &pb.TaskRun{
+				ApiVersion: "v1alpha1",
+				Metadata: &pb.ObjectMeta{
+					Uid:       "31415926",
+					Name:      "mytaskrun",
+					Namespace: "default",
+				},
+			},
+		},
+		{
+			in: &pb.TaskRun{
+				ApiVersion: "v1beta1",
+				Metadata: &pb.ObjectMeta{
+					Uid:       "31415926",
+					Name:      "mytaskrun",
+					Namespace: "default",
+				},
+				Spec: &pb.TaskRunSpec{
+					TaskSpec: &pb.TaskSpec{
+						Steps: []*pb.Step{
+							{
+								Name: "first_step",
+							},
+							{
+								Name: "next_step",
+							},
+						},
+					},
+				},
+			},
+			name:      "Test Mask updating repeated fields",
+			fieldmask: &field_mask.FieldMask{Paths: []string{"spec.task_spec.steps"}},
+			// update entire repeated field(all elements in array) - standard update
+			update: &pb.TaskRun{
+				ApiVersion: "v1beta1",
+				Metadata: &pb.ObjectMeta{
+					Uid:       "31415926",
+					Name:      "mytaskrun",
+					Namespace: "default",
+				},
+				Spec: &pb.TaskRunSpec{
+					TaskSpec: &pb.TaskSpec{
+						Steps: []*pb.Step{
+							{
+								Name: "new_first_step",
+							},
+							{
+								Name: "new_next_step",
+							},
+						},
+					},
+				},
+			},
+			expect: &pb.TaskRun{
+				ApiVersion: "v1beta1",
+				Metadata: &pb.ObjectMeta{
+					Uid:       "31415926",
+					Name:      "mytaskrun",
+					Namespace: "default",
+				},
+				Spec: &pb.TaskRunSpec{
+					TaskSpec: &pb.TaskSpec{
+						Steps: []*pb.Step{
+							{
+								Name: "new_first_step",
+							},
+							{
+								Name: "new_next_step",
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			in: &pb.TaskRun{
+				ApiVersion: "v1alpha1",
+				Metadata: &pb.ObjectMeta{
+					Uid:       "31415926",
+					Name:      "mytaskrun",
+					Namespace: "default",
+				},
+			},
+			name:      "Test Mask with nil Paths field",
+			fieldmask: &field_mask.FieldMask{},
+			// do not update
+			update: &pb.TaskRun{
+				ApiVersion: "v1beta1",
+				Metadata: &pb.ObjectMeta{
+					Uid:       "123456",
+					Name:      "newtaskrun",
+					Namespace: "tekton",
+				},
+			},
+			expect: &pb.TaskRun{
+				ApiVersion: "v1alpha1",
+				Metadata: &pb.ObjectMeta{
+					Uid:       "31415926",
+					Name:      "mytaskrun",
+					Namespace: "default",
+				},
+			},
+		},
 	}
-
-	// Validate by checking if we can get the updated taskrun
-	rows, err := srv.db.Query("SELECT taskrunlog FROM taskrun WHERE results_id = ?", r.GetResultsId())
-	if err != nil {
-		t.Fatalf("failed to query on database: %v", err)
-	}
-	for rows.Next() {
-		var taskrunblob []byte
-		taskrun := &pb.TaskRun{}
-		rows.Scan(&taskrunblob)
-		if err := proto.Unmarshal(taskrunblob, taskrun); err != nil {
-			t.Fatal("unmarshaling error: ", err)
-		}
-		if diff := cmp.Diff(taskrun.String(), r.GetTaskRun().String()); diff != "" {
-			t.Fatalf("Update Function not properly implemented: %v", diff)
-		}
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			r, err := srv.CreateTaskRunResult(ctx, &pb.CreateTaskRunRequest{TaskRun: tc.in})
+			if err != nil {
+				t.Fatalf("could not create taskrun: %v", err)
+			}
+			// Update the created taskrun
+			r, err = srv.UpdateTaskRunResult(ctx, &pb.UpdateTaskRunRequest{TaskRun: tc.update, ResultsId: r.GetResultsId(), UpdateMask: tc.fieldmask})
+			if err != nil && status.Code(err) != codes.NotFound {
+				t.Fatalf("could not update taskrun: %v, %v", err, status.Code(err))
+			}
+			rows, err := srv.db.Query("SELECT taskrunlog FROM taskrun WHERE results_id = ?", r.GetResultsId())
+			if err != nil {
+				t.Fatalf("failed to query on database: %v", err)
+			}
+			for rows.Next() {
+				var taskrunblob []byte
+				taskrun := &pb.TaskRun{}
+				rows.Scan(&taskrunblob)
+				if err := proto.Unmarshal(taskrunblob, taskrun); err != nil {
+					t.Fatal("unmarshaling error: ", err)
+				}
+				if diff := cmp.Diff(taskrun.String(), tc.expect.String()); diff != "" {
+					t.Fatalf("Update Function not properly implemented: %v, %v, %v", diff, tc.name, tc.fieldmask.String())
+				}
+			}
+		})
 	}
 }
 
@@ -186,7 +432,7 @@ func TestDeleteTaskRun(t *testing.T) {
 		t.Fatalf("failed to delete taskrun: %v", r.String())
 	}
 
-	// Check if a deleted taskrun can be delete again
+	// Check if a deleted taskrun can be deleted again
 	if _, err := srv.DeleteTaskRunResult(ctx, &pb.DeleteTaskRunRequest{ResultsId: r.GetResultsId()}); status.Code(err) != codes.NotFound {
 		t.Fatalf("same taskrun not supposed to be deleted again: %v", r.String())
 	}
