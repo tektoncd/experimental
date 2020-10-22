e90fff005200328828211ff7715407af3cf3af00
Xinru Zhang <xinruzhang@google.com>
2020-10-22 22:36:04 +0800

    Adds PipelineRun message to Results API
    1. Creates pipelinerun.proto file to store PipelineRun message
    2. Split out ObjectMeta and Condition messages to common.proto file
    to avoid repeat definitions
    3. Updates two dependencies:
        - google.golang.org/grpc v1.31.1 => v1.33.1
        - github.com/tektoncd/pipeline v0.12.0 => v0.17.1

--- results/proto/doc.go
+++ results/proto/doc.go
@@ -1,3 +1,3 @@
-//go:generate protoc --go_out=proto --go-grpc_out=proto --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative -I$GOPATH/src/github.com/googleapis/googleapis -I. api.proto taskrun.proto
+//go:generate protoc --go_out=proto --go-grpc_out=proto --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative -I$GOPATH/src/github.com/googleapis/googleapis -I. common.proto taskrun.proto pipelinerun.proto api.proto
 
 package proto
