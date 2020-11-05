ccda80f03fbcfaf7e386abf39606c8ef68861ad2
Billy Lynch <wlynch@google.com>
2020-11-05 18:23:06 -0500

    [results] Add e2e tests.
    This adds e2e tests that test basic k8s deployment and resource
    annotation. Tests use kind as the execution mechanism, but could also
    theoretically be ran against any k8s cluster.
    
    Also required the following changes for the e2e tests to pass:
    - Disables Knative leader election.
    - Sets real values in `config/` so that services can easily be brought
      up.
    - Configures mysql deployment to automatically create tables on startup.
    
    This does not add the tests to the repo automation yet. When I tried to
    run the presubmit_test.sh locally, the test hanged on the build tests.
    This will require further investigation, so holding off for now.

---
+++ results/test/e2e/install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Copyright 2020 The Tekton Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+export KO_DOCKER_REPO=${KO_DOCKER_REPO:-"kind.local"}
+
+ROOT="$(git rev-parse --show-toplevel)/results"
+
+echo "Installing Tekton Pipelines..."
+TEKTON_PIPELINE_CONFIG=${TEKTON_PIPELINE_CONFIG:-"https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml"}
+kubectl apply --filename ${TEKTON_PIPELINE_CONFIG}
+
+echo "Generating DB secret..."
+# Don't fail if the secret isn't created - this can happen if the secret already exists.
+kubectl create secret generic tekton-results-mysql --namespace="tekton-pipelines" --from-literal=user=root --from-literal=password=$(openssl rand -base64 20) || true
+
+echo "Generating DB init config..."
+kubectl create configmap mysql-initdb-config --from-file="${ROOT}/schema/results.sql" --namespace="tekton-pipelines" || true
+
+echo "Installing Tekton Results..."
+ko apply --filename="${ROOT}/config/"
+
+echo "Waiting for deployments to be ready..."
+kubectl wait deployment "tekton-results-mysql" --namespace="tekton-pipelines" --for="condition=available" --timeout="60s"
+kubectl wait deployment "tekton-results-api" --namespace="tekton-pipelines" --for="condition=available" --timeout="60s"
+kubectl wait deployment "tekton-results-watcher" --namespace="tekton-pipelines" --for="condition=available" --timeout="60s"
