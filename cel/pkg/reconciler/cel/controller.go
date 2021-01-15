bc7dce9f7da8ffae7e4b3e80f7347daa16cd32b9
Jerop <jerop@google.com>
2021-01-14 22:38:11 -0500

    Common Expression Language Custom Task
    This is the initial of an experimental project that provides support for
    Common Expression Language (CEL) in Tekton Pipelines.
    
    The functionality is provided by a controller that implements the
    Custom Task interface. Its use cases include evaluating complex
    expressions to be used in [`WhenExpressions`](https://github.com/tektoncd/pipeline/blob/master/docs/pipelines.md#guard-task-execution-using-whenexpressions)
    in subsequent `Tasks` to guard their execution.
    
    The `CEL` expressions to be evaluated by the `CELRun` are specified
    using parameters.
    
    As the `Run` executes, its `status` field accumulates information about
    the execution status of the `Run` in general. If the evaluation is
    successful, it will also contain the results of the evaluation under
    `status.results` with the corresponding names of the CEL expressions as
    provided in the parameters.

---
+++ cel/pkg/reconciler/cel/controller.go
@@ -0,0 +1,58 @@
+/*
+Copyright 2021 The Tekton Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cel
+
+import (
+	context "context"
+	run "github.com/tektoncd/pipeline/pkg/client/injection/informers/pipeline/v1alpha1/run"
+	v1alpha1run "github.com/tektoncd/pipeline/pkg/client/injection/reconciler/pipeline/v1alpha1/run"
+	tkncontroller "github.com/tektoncd/pipeline/pkg/controller"
+	"k8s.io/client-go/tools/cache"
+	configmap "knative.dev/pkg/configmap"
+	controller "knative.dev/pkg/controller"
+	logging "knative.dev/pkg/logging"
+)
+
+const (
+	ControllerName = "cel-controller"
+	apiVersion     = "cel.dev/v1alpha1"
+	kind           = "CEL"
+)
+
+// NewController creates a Reconciler for Run and returns the result of NewImpl.
+func NewController(ctx context.Context, cmw configmap.Watcher) *controller.Impl {
+	logger := logging.FromContext(ctx)
+
+	runInformer := run.Get(ctx)
+
+	r := &Reconciler{}
+
+	impl := v1alpha1run.NewImpl(ctx, r, func(impl *controller.Impl) controller.Options {
+		return controller.Options{
+			AgentName: ControllerName,
+		}
+	})
+
+	logger.Info("Setting up event handlers")
+
+	runInformer.Informer().AddEventHandler(cache.FilteringResourceEventHandler{
+		FilterFunc: tkncontroller.FilterRunRef(apiVersion, kind),
+		Handler:    controller.HandleAll(impl.Enqueue),
+	})
+
+	return impl
+}
