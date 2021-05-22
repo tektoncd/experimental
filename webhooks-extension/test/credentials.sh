04abacdba4cc09d634985399b7fa54752ef7179c
Alan Greene <github.com@alangreene.net>
2021-05-22 11:49:43 +0100

    [webhooks-extension] Delete the deprecated webhooks-extension
    webhooks-extension has been deprecated for over 9 months. Delete the
    folder and related test scripts/config.

--- webhooks-extension/test/credentials.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-# Credentials. Don't edit this and check it in. 
-DOCKERHUB_USERNAME=[your-dockerhub-id]
-DOCKERHUB_PASSWORD=[your-docker-hub-password]
-GITHUB_USERNAME=[your-github-login-id]
-GITHUB_TOKEN=[github-token-with-wehooks-permissions]
