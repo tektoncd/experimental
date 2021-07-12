a30fdba2bf3b191f502556c7f1165a394948529f
Andrea Frittoli <andrea.frittoli@gmail.com>
2021-07-12 21:57:50 +0100

    [cloudevents] Implement simple event key
    Add an initial simple implementation of an event key to be used
    as a key for the events cache.
    
    Signed-off-by: Andrea Frittoli <andrea.frittoli@gmail.com>

--- cloudevents/pkg/reconciler/events/cache/cache.go
+++ cloudevents/pkg/reconciler/events/cache/cache.go
@@ -2,8 +2,11 @@ package cache
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
+	"fmt"
 	cloudevents "github.com/cloudevents/sdk-go/v2"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // AddEventSentToCache adds the particular object to cache marking it as sent
@@ -12,7 +15,7 @@ func AddEventSentToCache(ctx context.Context, event *cloudevents.Event) error {
 	if cacheClient == nil {
 		return errors.New("cache client is nil")
 	}
-	cacheClient.Add(event.String(), nil)
+	cacheClient.Add(eventKey(event), nil)
 	return nil
 }
 
@@ -22,5 +25,26 @@ func IsCloudEventSent(ctx context.Context, event *cloudevents.Event) (bool, erro
 	if cacheClient == nil {
 		return false, errors.New("cache client is nil")
 	}
-	return cacheClient.Contains(event.String()), nil
+	return cacheClient.Contains(eventKey(event)), nil
+}
+
+// eventKey defines whether an event is considered different from another
+// in future we might want to let specific event types override this
+func eventKey(event *cloudevents.Event) string {
+	var (
+		data map[string]interface{}
+		resourceType string
+		resourceName string
+	)
+	json.Unmarshal(event.Data(), &data)
+	for k, v := range data {
+		if k == "pipelinerun" || k == "taskrun" {
+			resourceType = k
+			var meta metav1.ObjectMeta
+			json.Unmarshal([]byte(v.(string)), &meta)
+			resourceName = meta.Name
+		}
+	}
+	eventType := event.Type()
+	return fmt.Sprintf("%s/%s/%s", eventType, resourceType, resourceName)
 }
