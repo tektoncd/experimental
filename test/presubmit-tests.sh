d277f588060ac240d4263c777facd4baedd16386
Billy Lynch <wlynch@google.com>
2022-03-02 14:45:25 -0500

    [pipeline/trusted-resources] Add OWNERS file.
    Initial directory creation with initial OWNERS file. Proposed and
    approved in https://github.com/tektoncd/experimental/issues/834

--- test/presubmit-tests.sh
+++ test/presubmit-tests.sh
@@ -58,7 +58,7 @@ function should_test_folder() {
 # Get list of changed files
 initialize_environment
 
-projects="cel commit-status-tracker generators helm hub oci pipeline/cleanup pipelines-in-pipelines pipeline-to-taskrun tekdoc task-loops notifiers/github-app cloudevents"
+projects="cel commit-status-tracker generators helm hub oci pipeline/cleanup pipeline/trusted-resources pipelines-in-pipelines pipeline-to-taskrun tekdoc task-loops notifiers/github-app cloudevents"
 inanyprojectregex=$(echo "^${projects// /\/.* ^}\/.*" | sed 's/ /\\|/g')
 
 for proj in $projects; do
