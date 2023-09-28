90e8b513edc437477edf1805e478601b56001ce0
Rafael Leal <rafael.leal@nubank.com.br>
2023-09-28 13:50:53 -0300

    [metrics-operator] Add pipeline and pipelinerun monitors to runtime typer

--- metrics-operator/pkg/apis/monitoring/v1alpha1/register.go
+++ metrics-operator/pkg/apis/monitoring/v1alpha1/register.go
@@ -33,6 +33,10 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&TaskMonitorList{},
 		&TaskRunMonitor{},
 		&TaskRunMonitorList{},
+		&PipelineMonitor{},
+		&PipelineMonitorList{},
+		&PipelineRunMonitor{},
+		&PipelineRunMonitorList{},
 	)
 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
 	return nil
